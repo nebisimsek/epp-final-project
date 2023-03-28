d8471104693dd1e616a0824703747cc522d252a4
nebisimsek <s6nesims@uni-bonn.de>
2023-03-28 15:42:27 +0200

    clean the comments

--- paper/task_paper.py
+++ paper/task_paper.py
@@ -8,17 +8,3 @@ from epp_final_project.config import BLD, PAPER_DIR
 )
 def task_compile_latex_document():
     """Compile the document specified in the latex decorator."""
-
-
-# for document in documents:
-
-#     @pytask.mark.latex(
-#         ),
-#     @pytask.mark.task(id=document)
-#     def task_compile_document():
-#         """Compile the document specified in the latex decorator."""
-
-
-#     @pytask.mark.task(id=document, kwargs=kwargs)
-#     def task_copy_to_root(depends_on, produces):
-#         """Copy a document to the root directory for easier retrieval."""
